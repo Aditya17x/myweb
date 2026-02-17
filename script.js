5495a11aa05596ec1293cdf1d6dbcc01b380b5a4
Aditya Singh <127865088+Aditya17x@users.noreply.github.com>
2026-02-17 21:56:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+window.addEventListener("load", ()=>{
+    gsap.to("#loader",{opacity:0,duration:1,onComplete:()=>{
+        document.getElementById("loader").style.display="none";
+    }});
+});
+
+AOS.init();
+
+
+const roles = ["Creative Developer","UI Architect","Future Tech Overlord"];
+let i=0,j=0,current="",isDeleting=false;
+function type(){
+    if(i<roles.length){
+        if(!isDeleting && j<=roles[i].length){
+            current=roles[i].substring(0,j++);
+        }else if(isDeleting && j>=0){
+            current=roles[i].substring(0,j--);
+        }
+        document.querySelector(".typing").textContent=current;
+
+        if(j===roles[i].length) isDeleting=true;
+        if(j===0 && isDeleting){
+            isDeleting=false;
+            i++;
+        }
+    }
+    setTimeout(type,100);
+}
+type();
+
+
+document.querySelectorAll(".tilt").forEach(card=>{
+    card.addEventListener("mousemove",e=>{
+        let x=e.offsetX;
+        let y=e.offsetY;
+        let rotateX=(y/200)*15;
+        let rotateY=(x/200)*-15;
+        card.style.transform=`rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+    card.addEventListener("mouseleave",()=>{
+        card.style.transform="rotateX(0) rotateY(0)";
+    });
+});
+
+
+particlesJS("particles-js",{
+    particles:{
+        number:{value:80},
+        size:{value:3},
+        move:{speed:2},
+        line_linked:{enable:true}
+    }
+});
